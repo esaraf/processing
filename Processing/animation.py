299ea54c3211ef3f930eabcfa501505ac8b8919a
esaraf <elizabeth.saraf@beyondpricing.com>
2023-02-09 16:45:38 -0500

    Sine Curve Animated

--- Processing/animation.py
+++ Processing/animation.py
@@ -3,3 +3,20 @@ Purpose: Gain comfort in processing code (p5)
 Goal: Generate a moving circle
 '''
 
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+fig, ax = plt.subplots()
+
+x = np.arange(0, 2 * np.pi, 0.01)
+(line,) = ax.plot(x, np.sin(x))
+
+
+def animate(i):
+    line.set_ydata(np.sin(x + i / 10.0))  # update the data
+    return (line,)
+
+
+ani = animation.FuncAnimation(fig, animate, frames=100, interval=20, blit=True)
+plt.show()
